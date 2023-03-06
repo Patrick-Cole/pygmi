cae99a8ae55a0798d1c6d0d94b051f57b01b5562
Patrick <pcole@geoscience.org.za>
2023-03-06 15:23:28 +0200

    Update PyGMI.cmd

--- PyGMI.cmd
+++ PyGMI.cmd
@@ -9,8 +9,9 @@ echo If you do have any errors, you can send the message to:
 echo pcole@geoscience.org.za
 echo.
 echo Loading PyGMI...
-call ".\python\scripts\env.bat"
-python.exe quickstart.py > err.log 2>&1
+set GDAL_DATA=%cd%\python\Lib\site-packages\osgeo\data\gdal
+set PROJ_DATA=%cd%\python\Lib\site-packages\pyproj\proj_dir\share\proj
+.\python\python.exe quickstart.py > err.log 2>&1
 echo.
 echo Latest Errors and Messages:
 type err.log
