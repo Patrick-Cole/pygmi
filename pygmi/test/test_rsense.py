e6a30db89e2588b99f839993cfd7d62121bb3746
Patrick <pcole@geoscience.org.za>
2026-02-19 15:18:46 +0200

    Update test_rsense.py

--- pygmi/test/test_rsense.py
+++ pygmi/test/test_rsense.py
@@ -73,7 +73,8 @@ def test_pca():
     for i in dat[0]:
         for j in dat2:
             if i.dataid == j.dataid:
-                np.testing.assert_array_equal(np.abs(i.data), np.abs(j.data))
+                np.testing.assert_array_almost_equal(
+                    np.abs(i.data), np.abs(j.data))
                 break
 
 
@@ -93,7 +94,8 @@ def test_mnf():
     for i in dat[0]:
         for j in dat2:
             if i.dataid == j.dataid:
-                np.testing.assert_array_equal(np.abs(i.data), np.abs(j.data))
+                np.testing.assert_array_almost_equal(
+                    np.abs(i.data), np.abs(j.data))
                 break
 
 
